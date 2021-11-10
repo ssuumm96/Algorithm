067e3169a43b70819fe3ba50622a81694a6b2778
류선민 <0619dta@gmail.com>
2021-11-10 20:45:44 +0900

    Update dfs.py

--- Programers/dfs.py
+++ Programers/dfs.py
@@ -1,20 +1,25 @@
 def solution(numbers, target):
-    result = 0
-    def dfs(num, level):
-        nonlocal result
-        if level == len(numbers):
-            if num == target:
-                result += 1
+    #global answer
+    answer = 0
+    def dfs(num, level) :
+        nonlocal answer
+        #결과가 target이랑 같고 계산단계가 len(number)랑 같으면 리턴
+        if level == len(numbers) :
+            if num == target :
+                answer += 1
             return
-
-        signs = [-num, num]
-        if level == 1:
-            for i in range(2):
-                dfs(signs[i] + numbers[level], level + 1)
-                dfs(signs[i] - numbers[level], level + 1)
+        
+        #첫번째 수 +,-처리
+        firstNum = [-num,num]
+        
+        if level == 1 :
+            for i in firstNum:
+                dfs(i+numbers[level],level+1)
+                dfs(i-numbers[level],level+1)
         else:
-            dfs(num + numbers[level], level + 1)
-            dfs(num - numbers[level], level + 1)
-
+            dfs(num+numbers[level],level+1)
+            dfs(num-numbers[level],level+1)
+            
     dfs(numbers[0], 1)
-    return result
+    
+    return answer
